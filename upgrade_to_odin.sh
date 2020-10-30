90f5aebd37fd93af2e23456728d729b9c485c8d3
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2020-10-30 16:11:53 -0300

    Add files via upload

--- upgrade_to_odin.sh
+++ upgrade_to_odin.sh
@@ -17,12 +17,16 @@ sudo apt update
 
 sudo apt install elementary-tweaks
 
-sudo apt-get install sddm elementary-desktop elementary-tweaks indicator-application indicator-multiload pantheon-shell pantheon wingpanel plank lightdm-webkit-greeter  pantheon-terminal pantheon-xsession-settings contractor slingshot-launcher scratch elementary-theme elementary-icon-theme switchboard plank-theme-pantheon snap preload pantheon-file
+sudo apt-get install sddm elementary-desktop elementary-tweaks indicator-application indicator-multiload pantheon-shell pantheon wingpanel plank lightdm-webkit-greeter  pantheon-terminal pantheon-xsession-settings contractor slingshot-launcher scratch elementary-theme elementary-icon-theme switchboard plank-theme-pantheon snap preload pantheon-files
+
+sudo apt autoremove
 
 sudo apt install switchboard switchboard-gnome-control-center-override switchboard-plug-a11y switchboard-plug-about switchboard-plug-applications switchboard-plug-bluetooth switchboard-plug-datetime switchboard-plug-display switchboard-plug-gcc-wacom switchboard-plug-gcc-wacom-dbg switchboard-plug-keyboard switchboard-plug-locale switchboard-plug-mouse-touchpad switchboard-plug-networking switchboard-plug-notifications switchboard-plug-online-accounts switchboard-plug-pantheon-shell switchboard-plug-parental-controls switchboard-plug-power switchboard-plug-printers switchboard-plug-security-privacy switchboard-plug-sharing switchboard-plug-sound switchboard-plug-useraccounts switchboard-plug-users
 
+sudo apt autoremove
+
 sudo apt upgrade
 
-sudo dpkg-reconfigure ssdm
+sudo dpkg-reconfigure sddm
 
 
